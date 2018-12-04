03bcb4c01674d023be14b07ea7826c16ea21a792
andzajan <andzajan@gmail.com>
2018-12-04 18:20:22 +0000

    Reports include PCA scores plots per analytical batch

--- R/SignalBatchCorrection.R
+++ R/SignalBatchCorrection.R
@@ -50,7 +50,7 @@ SignalBatchCorrection <- function(QCreportObject)
 
   QCreportObject$plots$SBPCAbefore <- doPCA (Data=PCAinF2$Data, classes=PCAinF2$classes, PQN=T, mv_impute = T,
                                        glogScaling = T, scale=F,
-                labels="QC", qc_label = QCreportObject$QC_label, plotTitle = "PCA, blank and QC MV filtered")
+                labels=QCreportObject$pca_scores_labels, qc_label = QCreportObject$QC_label, plotTitle = "PCA, blank and QC MV filtered")
 
 
   QCreportObject$plots$SBPCAbeforeQC <- doPCA (Data=PCAinQC2$Data, classes=PCAinQC2$classes, PQN=F, mv_impute = T,
@@ -61,11 +61,65 @@ SignalBatchCorrection <- function(QCreportObject)
 
   QCreportObject$plots$SBRSDbefore <- doRSDplot(RSD=PCAinF2$RSD, plotTitle = "RSD (%) per sample group, blank and QC MV filtered")
 
+  
+  # If multiple batches are present create PCA plots per batch before correction
+  nbatches <- unique(QCreportObject$metaD$table$batch)
+    
+  if (length (unique(QCreportObject$metaD$table$batch))>1){
+    
+    title_list <- classes_list <- data_list <- vector("list", length(nbatches))
+    
+    for (batch in 1:length(nbatches)){
+      
+      batch_hits <- which (QCreportObject$metaData$table$batch == nbatches[batch])
+      data_list[[batch]] <- QCreportObject$data$dataMatrix[ , batch_hits]
+      classes_list[[batch]] <- class[batch_hits]
+      title_list[[batch]] <- paste ("PCA, blank and QC MV filtered,", "Batch", nbatches[batch])
+    }
+    
+    title_list <- unlist (title_list)
+    
+    plots_per_batch <- pmp::doSummaryPlot(Data = data_list, 
+                                          classes =classes_list, 
+                                          plotTitle = title_list,
+                                          PQN=T, 
+                                          glogScaling=T, mv_impute=T, scale=F, plot = F, labels = "all", 
+                                          blank=QCreportObject$Blank_label, qc_label = QCreportObject$QC_label)
+    
+    # pca plots are all odd numbers nad RSD even numbers
+    QCreportObject$plots$plots_per_batch_pca <- plots_per_batch[seq(1, length(nbatches)*2, 2)] 
+    QCreportObject$plots$plots_per_batch_rsd <- plots_per_batch[seq(2, length(nbatches)*2, 2)]   
+    
+  }
+  
+  #S/B correction
 
-  SBcorrected <- doQCRLSC(Data=MV_filtered, order=QCreportObject$metaData$table$injection_order,
+  SBcorrected <- sbcms::doQCRLSC(Data=MV_filtered, order=QCreportObject$metaData$table$injection_order,
                         batch=QCreportObject$metaData$table$batch, classes=class,
                         loessSpan=0, minQC = 5)
+  
+  # Temporary, to validate error from old pqn code
+  #source("C:/Users/jankevia/Downloads/normalisation.R")
+  
+  #sub_hits <- which (class=="Removed")
+  #sub_hits <- append (sub_hits, which(class=="Blank"))
+  
+  #class_sub <- class[-c(sub_hits)]
+  
+  #data_sub <- SBcorrected[,-c(sub_hits)]
+  
+  #wrong_pqn <- pqn_normalisation(df=t(data_sub), classes = class_sub, qc_label = "QC")
+  
+  #PCAinSB <- prepareData(Data=wrong_pqn$df, classes=class_sub, blank = "Blank",
+  #                       PQN=F, mv_impute = T, glogScaling = T,
+  #                       qc_label = QCreportObject$QC_label, ignorelabel = "Removed")
 
+  #QCreportObject$plots$SBPCAfter_pqn_wrong <- doPCA (Data=PCAinSB$Data, classes=PCAinSB$classes, PQN=T, mv_impute = T,
+  #                                         glogScaling = T, scale=F,
+  #                                         labels="QC", qc_label = QCreportObject$QC_label, 
+  #                                         plotTitle = "PCA, blank and QC MV filtered, S/B corrected")
+  
+    
   PCAinSB <- prepareData(Data=SBcorrected, classes=class, blank = QCreportObject$Blank_label,
                        PQN=T, mv_impute = T, glogScaling = T,
                        qc_label = QCreportObject$QC_label, ignorelabel = "Removed")
@@ -74,11 +128,11 @@ SignalBatchCorrection <- function(QCreportObject)
                        PQN=F, mv_impute = T, glogScaling = F,
                        qc_label = NULL, ignorelabel = "Removed")
 
-  QCreportObject$plots$SBPCAafter <- doPCA (Data=PCAinSB$Data, classes=PCAinSB$classes, PQN=T, mv_impute = T,
+  QCreportObject$plots$SBPCAfter <- doPCA (Data=PCAinSB$Data, classes=PCAinSB$classes, PQN=T, mv_impute = T,
                                           glogScaling = T, scale=F,
-       labels="QC", qc_label = QCreportObject$QC_label, plotTitle = "PCA, blank and QC MV filtered, S/B corrected")
+       labels="none", qc_label = QCreportObject$QC_label, plotTitle = "PCA, blank and QC MV filtered, S/B corrected")
 
-  QCreportObject$plots$SBPCAafterQC <- doPCA (Data=PCAinSBQC$Data, classes=PCAinSBQC$classes, PQN=F, mv_impute = T,
+  QCreportObject$plots$SBPCAfterQC <- doPCA (Data=PCAinSBQC$Data, classes=PCAinSBQC$classes, PQN=F, mv_impute = T,
                                        glogScaling = F, scale= T,
                                   labels="QC", qc_label = QCreportObject$QC_label, plotTitle = "PCA, blank and QC MV filtered, S/B corrected")
 

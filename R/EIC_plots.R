2ebcf68354598850020cc5783e14260b77603b42
andzajan <andzajan@gmail.com>
2019-04-02 13:40:25 +0100

    Fixed bug with EIC tool trying to extract chromatograms outside of measured range

--- R/EIC_plots.R
+++ R/EIC_plots.R
@@ -17,11 +17,9 @@ XCMS_EIC_plot <- function (indexes, rawfiles, class, xset, Blank_label="Blank", 
 {
 
   # Check if RT correction has been applied
-  if (!all(unlist(xset@rt$raw)==unlist(xset@rt$corrected)))
-  {
+  if (!all(unlist(xset@rt$raw)==unlist(xset@rt$corrected))) {
     RTcorrected = T
-  } else
-  {
+  } else {
     RTcorrected = F
   }
 
@@ -29,32 +27,29 @@ XCMS_EIC_plot <- function (indexes, rawfiles, class, xset, Blank_label="Blank", 
   rtmin = xcms::groupval(object=xset, method = "medret", value = "rtmin", intensity = "into")
   rtmax = xcms::groupval(object=xset, method = "medret", value = "rtmax", intensity = "into")
 
-  min_rt = rtmin[indexes,]
-  max_rt = rtmax[indexes,]
+  min_rt = rbind(rtmin[indexes, ], NULL)
+  max_rt = rbind(rtmax[indexes, ], NULL)
 
   mzmin = xcms::groupval(object=xset, method = "medret", value = "mzmin", intensity = "into")
   mzmax = xcms::groupval(object=xset, method = "medret", value = "mzmax", intensity = "into")
 
-  min_mz = mzmin[indexes,]
-  max_mz = mzmax[indexes,]
+  min_mz = rbind(mzmin[indexes,], NULL)
+  max_mz = rbind(mzmax[indexes,], NULL)
 
   chromPlots <- vector ("list", length(indexes))
 
-  for (chromind in 1:length(indexes))
-  {
+  for (chromind in 1:length(indexes)) {
     cat ("EIC: ", chromind, " ... sample: ")
     chroms <- vector("list",length(rawfiles))
 
-    for (chrn in 1:length(rawfiles))
-    {
+    for (chrn in 1:length(rawfiles)) {
       cat (chrn, " ")
       mzmi <- min_mz[chromind, chrn]
       mzma <- max_mz[chromind, chrn]
       RTmi <- min_rt[chromind, chrn]
       RTma <- max_rt[chromind, chrn]
 
-      if (RTcorrected==T)
-      {
+      if (RTcorrected==T) {
         RTind <- c(NA,NA)
         RTind[1] <- which(xset@rt$corrected[[chrn]]>=RTmi)[1]
         RTind[2] <- which(xset@rt$corrected[[chrn]]>=RTma)[1]
@@ -63,18 +58,15 @@ XCMS_EIC_plot <- function (indexes, rawfiles, class, xset, Blank_label="Blank", 
         RTma <- xset@rt$raw[[chrn]][RTind[2]]
       }
 
-      if (!all(is.na(c(mzmi,mzma,RTmi,RTma))) && (!is.null(rawfiles[[chrn]])))
-      {
+      if (!all(is.na(c(mzmi,mzma,RTmi,RTma))) && (!is.null(rawfiles[[chrn]]))) {
         chroms[[chrn]] <- xcms::extractMsData (rawfiles[[chrn]], mz=c(mzmi, mzma), rt=c(RTmi,RTma), msLevel=1)[[1]]
 
         # Replace raw RT with corrected if RT correction was applied
-        if (nrow(chroms[[chrn]])!=0 && RTcorrected==T)
-        {
+        if (nrow(chroms[[chrn]])!=0 && RTcorrected==T) {
           RThits <- which(xset@rt$raw[[chrn]] %in% chroms[[chrn]]$rt)
 
           # For some weird reason, extracMsData can return mor than one peak for the same RT
-          if (length(RThits)!=nrow(chroms[[chrn]]))
-          {
+          if (length(RThits)!=nrow(chroms[[chrn]])) {
               RThits <- rownames (chroms[[chrn]])
               RThits <- sub("F1.S0", "", RThits)
               RThits <- round(as.numeric(RThits),0)
@@ -86,8 +78,7 @@ XCMS_EIC_plot <- function (indexes, rawfiles, class, xset, Blank_label="Blank", 
 
         # To keeps track on class and sample labels, we need to have empty data frame, if there are no EIC's to extract
         if (nrow(chroms[[chrn]])==0) chroms[[chrn]] <- data.frame(rt=NA, mz=NA, i=NA)
-      } else
-      {
+      } else {
         chroms[[chrn]] <- data.frame(rt=NA, mz=NA, i=NA)
       }
     }

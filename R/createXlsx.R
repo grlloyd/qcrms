35f140e8f07794b496b9d62c18ea344ff85b7c57
andzajan <andzajan@gmail.com>
2019-06-13 15:18:00 +0100

    createXlsx is using correct object name in case of text file input.

--- R/createXlsx.R
+++ R/createXlsx.R
@@ -78,7 +78,7 @@ createXlsx <- function (QCreportObject)
  
   } else {
     #QCreportObject$data$dataMatrix = as.matrix(read.table(QCreportObject$data_file, header=TRUE, row.names=1, check.names=FALSE))
-    QCreportObject$peakMatrix[QCreportObject$peakMatrix == 0] <= NA
+    QCreportObject$peakMatrix[QCreportObject$peakMatrix == 0] <- NA
   }
   
   # Write out meta data
@@ -90,8 +90,8 @@ createXlsx <- function (QCreportObject)
   QCreportObject$metaData$table[,QCreportObject$metaData$classColumn] <- QCreportObject$metaData$samp_lab
   
   # W4M for no good reason decided to check if file names contains "-" and similar symbols. Why?
-  # If it xcms can handle original file names, and after al files have been processed.
-  # Where is tracability to the original files if this has been done to outputs?
+  # If it xcms can handle original file names, and after all files have been processed.
+  # Where is tracability to the original files if this has been done to the outputs?
 
   if (!all(colnames(QCreportObject$data$dataMatrix) == QCreportObject$metaData$table$Sample)){
     
@@ -115,7 +115,7 @@ createXlsx <- function (QCreportObject)
   } else
   {
     addWorksheet (wb, "variableMetaData")
-    variableMetaData = rowMeans(QCreportObject$data$dataMatrix, na.rm = TRUE, dims = 1)
+    variableMetaData = rowMeans(QCreportObject$peakMatrix, na.rm = TRUE, dims = 1)
     print(data.frame(mz=names(variableMetaData), intensity=as.vector(variableMetaData)))
     writeData (wb, "variableMetaData", data.frame(mz=names(variableMetaData), intensity=as.vector(variableMetaData)), rowNames = F)
   }  

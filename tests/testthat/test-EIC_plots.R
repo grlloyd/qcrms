500960e2220eabc7a97612d7e147b04a6a99ce42
andzajan <andzajan@gmail.com>
2020-03-26 19:43:39 +0000

    Fix for EIC unit test.

--- tests/testthat/test-EIC_plots.R
+++ tests/testthat/test-EIC_plots.R
@@ -1,20 +1,27 @@
 context ("Test EIC plot.")
 
-mzml_files <- dir(system.file("extdata/lcms/mzML", package="msPurityData"),
-    full.names=TRUE, recursive=TRUE)
-rawf <- MSnbase::readMSData(mzml_files[1:2], mode="onDisk", msLevel.=1)
-cwp <- xcms::CentWaveParam(snthresh=10)
-xset <- expect_warning(xcms::findChromPeaks(rawf, param=cwp))
-xset <- xcms::groupChromPeaks(xset,
-    xcms::PeakDensityParam(sampleGroups=c("1", "1")))
-xset <- as(xset, "xcmsSet")
+test_that("EIC plot is created and has correct data", {
+    require(xcms)
+    
+    mzml_files <- dir(system.file("extdata/lcms/mzML", package="msPurityData"),
+        full.names=TRUE, recursive=TRUE)
+    rawf <- MSnbase::readMSData(mzml_files[1:2], mode="onDisk", msLevel.=1)
+    cwp <- xcms::CentWaveParam(snthresh=10)
+    xset <- expect_warning(xcms::findChromPeaks(rawf, param=cwp))
+    xset <- xcms::groupChromPeaks(xset,
+        xcms::PeakDensityParam(sampleGroups=c("1", "1")))
+    xset <- as(xset, "xcmsSet")
 
-rawfiles <- list()
-rawfiles[[1]] <- MSnbase::readMSData(mzml_files[1], mode="onDisk", msLevel.=1)
-rawfiles[[2]] <- MSnbase::readMSData(mzml_files[2], mode="onDisk", msLevel.=1)
+    rawfiles <- list()
+    rawfiles[[1]] <- MSnbase::readMSData(mzml_files[1], mode="onDisk", 
+        msLevel.=1)
+    rawfiles[[2]] <- MSnbase::readMSData(mzml_files[2], mode="onDisk", 
+        msLevel.=1)
 
-plot <- expect_warning(qcrms:::XCMS_EIC_plot(indexes=100, rawfiles=rawfiles, 
-    xset=xset, class=c("1", "1")))
+    plot <- expect_warning(qcrms:::XCMS_EIC_plot(indexes=100,
+        rawfiles=rawfiles, xset=xset, class=c("1", "1")))
 
-#expect_equal(round(mean(plot[[1]]$data$rt),4), 44.0588)
-#expect_equal(round(mean(plot[[1]]$data$mz),4), 146.1173)
+    # Fails on devtools:test() works with testhat
+    #expect_equal(round(mean(plot[[1]]$data$rt),4), 44.0588)
+    #expect_equal(round(mean(plot[[1]]$data$mz),4), 146.1173)
+})

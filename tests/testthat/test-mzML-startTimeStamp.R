38c18802c155e8ce80df9a13d4375e74c1d2fad7
andzajan <andzajan@gmail.com>
2020-04-27 15:00:34 +0100

    mzML timestamp: Unit test to check if function works when timestamp is not present in mzML file.

--- tests/testthat/test-mzML-startTimeStamp.R
+++ tests/testthat/test-mzML-startTimeStamp.R
@@ -1,16 +1,32 @@
 context ("Test start time stamp extraction from mzML files.")
 
-mzml_files <- dir(system.file("extdata/dims/mzML", package = "msPurityData"),
-    full.names = TRUE, recursive = TRUE)
+test_that("Timestamp gets extracted", {
+    mzml_files <- dir(system.file("extdata/dims/mzML",
+        package = "msPurityData"), full.names = TRUE, recursive = TRUE)
+    time_stamp <- qcrms:::mzML.startTimeStamp(mzml_files[1])
+    expect_equal(time_stamp, "2015-07-10 15:25:01Z")
+})
 
-time_stamp <- qcrms:::mzML.startTimeStamp(mzml_files[1])
+test_that("Warninn message is created if files are not mzML", {
+    mzml_files <- dir(system.file("cdf", package = "faahKO"),
+        full.names = TRUE, recursive = TRUE)
+    expect_message(expect_warning(time_stamp <-
+        qcrms:::mzML.startTimeStamp(mzml_files[1])))
+})
 
-expect_equal(time_stamp, "2015-07-10 15:25:01Z")
-
-context("Message of times stamp not avaliable is given.")
-
-mzml_files <- dir(system.file("cdf", package = "faahKO"),
-    full.names = TRUE, recursive = TRUE)
-
-expect_message(expect_warning(time_stamp <-
-    qcrms:::mzML.startTimeStamp(mzml_files[1])))
+test_that ("If run block doesn't contain timeStamp, function stops", {
+    mzml_files <- dir(system.file("extdata/dims/mzML",
+        package = "msPurityData"), full.names = TRUE, recursive = TRUE)
+    con <- file(mzml_files[1], "r")
+    mzml_content <- readLines(con)
+    mzml_content[90] <- sub (" startTimeStamp=\"2015-07-10T15:25:01Z\"", "",
+        mzml_content[90])
+    close (con)
+    output_file <- file.path(tempdir(), "out.mzML")
+    con <- file(output_file, "w")
+    writeLines(con, text=mzml_content)
+    close (con)
+    time_stamp <- qcrms:::mzML.startTimeStamp(output_file)
+    expect_true(is.na(time_stamp))
+    file.remove(output_file)
+})

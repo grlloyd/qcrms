c9999bb1c42473e56a27c8796161a47882e2cfde
andzajan <andzajan@gmail.com>
2020-05-22 23:12:41 +0100

    EIC functions support both xcms output object classes.

--- R/EIC_plots.R
+++ R/EIC_plots.R
@@ -1,8 +1,64 @@
-#' @importFrom  xcms groupval
+#' @importFrom xcms groupval
+#' @importFrom xcms featureValues
 #' @import ggplot2
 #' @importFrom magrittr %>%
 #'
 NULL
+
+#' Check if retention time correction was applied during xcms processing.
+#'
+#' @param xset XCMS object from which to extrat chromatograms.
+#' @return logicla(1), TRUE if retention time correction has been applied.
+
+check_rt_correction <- function(xset){
+    rt_corrected <- FALSE
+    if (is(xset, "XCMSnExp")){
+        rt_corrected <- hasAdjustedRtime(xset)
+    }
+    if (is (xset, "xcmsSet")){
+        if (!all(unlist(xset@rt$raw) == unlist(xset@rt$corrected))){
+            rt_corrected <- TRUE
+        }
+    }
+    rt_corrected
+}
+
+#' Return summaries of XCMS grouping RT and mz values per sample.
+#' 
+#' @param xset XCMS object from which to extrat chromatograms.
+#' @param indexes vector of feautre indices to extract
+
+xcms_groups_rt_mz_summary <- function(xset, indexes){
+    groups_rt_mz_list <- list()
+    if(is(xset, "xcmsSet")){
+        # Actual min/max rt, mz and intensities for all samples
+        rtmin <- xcms::groupval(object=xset, method="medret", value="rtmin",
+            intensity="into")
+        rtmax <- xcms::groupval(object=xset, method="medret", value="rtmax",
+            intensity="into")
+        mzmin <- xcms::groupval(object=xset, method="medret", value="mzmin",
+            intensity="into")
+        mzmax <- xcms::groupval(object=xset, method="medret", value="mzmax",
+            intensity="into")
+    }
+    if(is(xset, "XCMSnExp")){
+        rtmin <- xcms::featureValues(object=xset, method="medret",
+            value="rtmin", intensity="into")
+        rtmax <- xcms::featureValues(object=xset, method="medret",
+            value="rtmax", intensity="into")
+        mzmin <- xcms::featureValues(object=xset, method="medret",
+            value="mzmin", intensity="into")
+        mzmax <- xcms::featureValues(object=xset, method="medret",
+            value="mzmax", intensity="into")
+    }
+
+    groups_rt_mz_list$min_rt <- rtmin[indexes, , drop=FALSE]
+    groups_rt_mz_list$max_rt <- rtmax[indexes, , drop=FALSE]
+    groups_rt_mz_list$min_mz <- mzmin[indexes, , drop=FALSE]
+    groups_rt_mz_list$max_mz <- mzmax[indexes, , drop=FALSE]
+
+    groups_rt_mz_list
+}
 
 #' Exctract and return ggplot2 object of EICs for specified xcms features
 #'
@@ -18,28 +74,9 @@ XCMS_EIC_plot <- function(indexes, rawfiles, class, xset, Blank_label="Blank",
   QC_label="QC") {
 
   # Check if RT correction has been applied
-  if (!all(unlist(xset@rt$raw) == unlist(xset@rt$corrected))) {
-    RTcorrected <- TRUE
-  } else {
-    RTcorrected <- FALSE
-  }
+  rt_corrected <- check_rt_correction(xset=xset)
 
-  # Actual min/max rt, mz and intensities for all samples
-  rtmin <- xcms::groupval(object=xset, method="medret", value="rtmin",
-    intensity="into")
-  rtmax <- xcms::groupval(object=xset, method="medret", value="rtmax",
-    intensity="into")
-
-  min_rt <- rbind(rtmin[indexes, ], NULL)
-  max_rt <- rbind(rtmax[indexes, ], NULL)
-
-  mzmin <- xcms::groupval(object=xset, method="medret", value="mzmin",
-    intensity="into")
-  mzmax <- xcms::groupval(object=xset, method="medret", value="mzmax",
-    intensity="into")
-
-  min_mz <- rbind(mzmin[indexes, ], NULL)
-  max_mz <- rbind(mzmax[indexes, ], NULL)
+  peaks_rt_mz_list <- xcms_groups_rt_mz_summary(xset = xset, indexes = indexes)
 
   chromPlots <- vector("list", length(indexes))
 
@@ -49,13 +86,13 @@ XCMS_EIC_plot <- function(indexes, rawfiles, class, xset, Blank_label="Blank",
 
     for (chrn in seq_len(length(rawfiles))) {
       cat(chrn, " ")
-      mzmi <- min_mz[chromind, chrn]
-      mzma <- max_mz[chromind, chrn]
-      RTmi <- min_rt[chromind, chrn]
-      RTma <- max_rt[chromind, chrn]
+      mzmi <- peaks_rt_mz_list$min_mz[chromind, chrn]
+      mzma <- peaks_rt_mz_list$max_mz[chromind, chrn]
+      RTmi <- peaks_rt_mz_list$min_rt[chromind, chrn]
+      RTma <- peaks_rt_mz_list$max_rt[chromind, chrn]
 
       # Find index of corrected RT and replace RT with raw value
-      if (RTcorrected == TRUE) {
+      if (rt_corrected == TRUE) {
         RTind <- c(NA, NA)
         RTind[1L] <- which(xset@rt$corrected[[chrn]] >= RTmi)[1L]
         RTind[2L] <- which(xset@rt$corrected[[chrn]] >= RTma)[1L]
@@ -78,7 +115,7 @@ XCMS_EIC_plot <- function(indexes, rawfiles, class, xset, Blank_label="Blank",
                 # remove file column
                 chroms[[chrn]]$file <- NULL
                 # Replace raw RT with corrected if RT correction was applied
-                if (nrow(chroms[[chrn]]) != 0L && RTcorrected == TRUE) {
+                if (nrow(chroms[[chrn]]) != 0L && rt_corrected == TRUE) {
                     RThits <- which(xset@rt$raw[[chrn]] %in% chroms[[chrn]]$rt)
                 if (length(RThits) > 0L & length(RThits) == 
                     nrow(chroms[[chrn]])) {
@@ -108,8 +145,10 @@ XCMS_EIC_plot <- function(indexes, rawfiles, class, xset, Blank_label="Blank",
     A <- do.call(rbind, chroms)
 
     ptitle <- paste("EICs, m/z ",
-      round(stats::median(min_mz[chromind, ], na.rm=TRUE), 5L), "-",
-      round(stats::median(max_mz[chromind, ], na.rm=TRUE), 5L), sep="")
+      round(stats::median(peaks_rt_mz_list$min_mz[chromind, ],
+          na.rm=TRUE), 5L), "-",
+      round(stats::median(peaks_rt_mz_list$max_mz[chromind, ],
+          na.rm=TRUE), 5L), sep="")
 
     # Reorder factor levels again, so that colors are consistent
     plotCols <- createClassAndColors(class=A$Class, QC_label=QC_label,
--- tests/testthat/test-EIC_plots.R
+++ tests/testthat/test-EIC_plots.R
@@ -10,24 +10,48 @@ test_that("EIC plot is created and has correct data, using XCMSnExp class.", {
     mzml_files <- dir(system.file("extdata/lcms/mzML", package="msPurityData"),
         full.names=TRUE, recursive=TRUE)
     rawf <- MSnbase::readMSData(mzml_files[1:2], mode="onDisk", msLevel.=1)
-    cwp <- xcms::CentWaveParam(snthresh=10)
+    cwp <- xcms::CentWaveParam(snthresh=10, noise=5000)
     xset <- xcms::findChromPeaks(rawf, param=cwp)
     xset <- xcms::groupChromPeaks(xset,
-        xcms::PeakDensityParam(sampleGroups=c("1", "1")))
-    xset <- as(xset, "xcmsSet")
-
+        xcms::PeakDensityParam(sampleGroups=c("mzML", "mzML")))
+ 
     rawfiles <- list()
     rawfiles[[1]] <- MSnbase::readMSData(mzml_files[1], mode="onDisk", 
         msLevel.=1)
     rawfiles[[2]] <- MSnbase::readMSData(mzml_files[2], mode="onDisk", 
         msLevel.=1)
 
+    expect_false(qcrms:::check_rt_correction(xset))
+    
+    expect_equal(qcrms:::xcms_groups_rt_mz_summary(xset = xset, indexes = 100),
+        list(min_rt = structure(c(73.060488, 72.332784), .Dim = 1:2,
+            .Dimnames = list("FT0100", c("LCMS_1.mzML", "LCMS_2.mzML"))),
+            max_rt = structure(c(97.176492, 95.748174), .Dim = 1:2,
+            .Dimnames = list("FT0100", c("LCMS_1.mzML", "LCMS_2.mzML"))),
+            min_mz = structure(c(152.988571166992, 152.98844909668),
+            .Dim = 1:2, .Dimnames = list("FT0100", 
+                c("LCMS_1.mzML", "LCMS_2.mzML"))),
+            max_mz = structure(c(152.990859985352, 152.991241455078),
+            .Dim = 1:2, .Dimnames = list("FT0100",
+                c("LCMS_1.mzML", "LCMS_2.mzML"))))
+    )
+    
     plot <- expect_warning(qcrms:::XCMS_EIC_plot(indexes=100,
         rawfiles=rawfiles, xset=xset, class=c("1", "1")))
+    
+    expect_equal(head(plot[[1]]$data),
+        structure(list(
+            rt = c(73.381104, 73.70211, 74.34549, 74.66874, 74.992116,
+                75.642732),
+            mz = c(152.990295410156, 152.989715576172, 152.99055480957,
+                152.988983154297, 152.990280151367, 152.988830566406),
+            i = c(21548.203125, 59140.6796875, 61134.80078125, 37297.19140625,
+                54743.37109375, 22704.90234375),
+            Class = structure(c(1L, 1L, 1L, 1L, 1L, 1L), .Label = "1",
+                class = c("ordered", "factor")), 
+            sample = c(1L, 1L, 1L, 1L, 1L, 1L)),
+        row.names = c(NA, 6L), class = "data.frame"))
 
-    # Fails on devtools:test() works with testhat
-    #expect_equal(round(mean(plot[[1]]$data$rt),4), 44.0588)
-    #expect_equal(round(mean(plot[[1]]$data$mz),4), 146.1173)
 })
 
 test_that("EIC plot is created and has correct data, using xcmsSet class.", {
@@ -46,8 +70,21 @@ test_that("EIC plot is created and has correct data, using xcmsSet class.", {
     rawfiles[[2]] <- MSnbase::readMSData(mzml_files[2], mode="onDisk", 
         msLevel.=1)
 
-    plot <- expect_warning(qcrms:::XCMS_EIC_plot(indexes=100,
-        rawfiles=rawfiles, xset=xset, class=c("1", "1")))
+    expect_false(qcrms:::check_rt_correction(xset))
+    
+    expect_equal(qcrms:::xcms_groups_rt_mz_summary(xset = xset, indexes = 100),
+        list(min_rt = structure(c(73.060488, 72.332784), .Dim = 1:2,
+            .Dimnames = list("153/79", c("LCMS_1", "LCMS_2"))),
+            max_rt = structure(c(97.176492, 95.748174), .Dim = 1:2,
+            .Dimnames = list("153/79", c("LCMS_1", "LCMS_2"))),
+            min_mz = structure(c(152.988571166992, 152.98844909668),
+            .Dim = 1:2, .Dimnames = list("153/79", c("LCMS_1", "LCMS_2"))),
+            max_mz = structure(c(152.990859985352, 152.991241455078),
+            .Dim = 1:2, .Dimnames = list("153/79", c("LCMS_1", "LCMS_2"))))
+    )
+    
+    plot <- expect_warning(qcrms:::XCMS_EIC_plot(indexes=100, rawfiles=rawfiles,
+        xset=xset, class=c("1", "1")))
 
     expect_equal(head(plot[[1]]$data),
         structure(list(
@@ -61,4 +98,25 @@ test_that("EIC plot is created and has correct data, using xcmsSet class.", {
                 class = c("ordered", "factor")), 
             sample = c(1L, 1L, 1L, 1L, 1L, 1L)),
         row.names = c(NA, 6L), class = "data.frame"))
+    
+    context ("EIC on RT corrected data")
+    
+    xset <- xcms::retcor(object = xset, method = "obiwarp")
+    expect_true(qcrms:::check_rt_correction(xset))
+    xset <- xcms::group(xset, method = "density", bw = 30, minfrac = 0.5,
+        minsamp = 1, mzwid=0.25, max=50)
+    plot <- expect_warning(qcrms:::XCMS_EIC_plot(indexes = 100,
+        rawfiles = rawfiles, xset = xset, class = c("1", "1")))
+    expect_equal(plot[[1]]$data[70:74, ],
+        structure(list(
+            rt = c(93.9810333251953, 94.3161697387695, 94.6508255004883, 
+                94.9869689941406, 95.3240203857422),
+            mz = c(152.990341186523, 152.988815307617, 152.990951538086,
+                152.988845825195, 152.990097045898),
+            i = c(32439.16796875, 32364.900390625, 38738.38671875,
+                33542.52734375, 40061.76171875),
+            Class = structure(c(1L, 1L, 1L, 1L, 1L), .Label = "1",
+                class = c("ordered", "factor")), 
+            sample = c(2L, 2L, 2L, 2L, 2L)),
+        row.names = 70:74, class = "data.frame"))
 })
